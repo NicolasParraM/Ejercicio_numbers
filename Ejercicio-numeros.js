685ddff59033e30f358e09e828e5dddd81550e78
Nicolas Parra <thndrdbeta@gmail.com>
2022-10-07 00:27:35 -0500

    Ejercicio Numbers

---
+++ Ejercicio-numeros.js
@@ -0,0 +1,9 @@
+let altura = 40;
+let Estatura = 1.80;
+let peso = 55.33
+
+let altura_red = Math.ceil(altura);
+let peso_red = Math.floor(peso);
+
+let valor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(valor);
